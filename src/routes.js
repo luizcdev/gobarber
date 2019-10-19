e28ea6428bc154a002d4413be5e00b4dba7b53c3
Luiz Oliveira <luizcdev@gmail.com>
2019-10-19 13:10:54 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Luiz Oliveira',
+    email: 'luizcdev@gmail.com',
+    password_hash: '123',
+  });
+
+  res.json(user);
+});
+
 export default routes;
