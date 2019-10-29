08cb5648dd90114ea59ee71bead802a9407c09d4
Luiz Oliveira <luizcdev@gmail.com>
2019-10-28 23:36:27 -0300

    Listagem de prestadores de serviço

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,6 +16,7 @@ routes.use(authMiddleware);
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
 
+routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
